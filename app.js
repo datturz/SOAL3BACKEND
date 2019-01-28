095c201d67b6516e8fb74c3617f0e4f3da5e06c0
hidatturz <hidayattur07@gmail.com>
2019-01-28 15:24:09 +0700

    soal3

---
+++ app.js
@@ -0,0 +1,13 @@
+let express = require('express')
+let app = express()
+let cors = require('cors')
+let router = require('./routes/router')
+app.use(cors())
+app.use(router)
+app.get('/', (req, res) => {
+    res.send('<h1>SOAL 2 BACKEND</h1>')
+})
+
+app.listen(1234, () => {
+    console.log('server running')
+})
